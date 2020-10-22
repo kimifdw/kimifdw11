a99d15792c5620cb1faa97acae60c2645dcf75dd
fudongwei <fdw911cool@qq.com>
2020-10-22 15:59:37 +0800

    title名修改

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -37,7 +37,7 @@ function SEO({ meta, image, title, description, slug, lang = 'en' }) {
                   title,
                 }
               : {
-                  title: `${siteMetadata.title} — A blog by XueSeason`,
+                  title: `${siteMetadata.title} — A blog by Kimifdw`,
                 })}
             meta={[
               {
